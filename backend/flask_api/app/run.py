801b68eb244f268b5be4747d9498d053dc892364
JIACHENG135 <liulaoye135@tamu.edu>
2020-10-10 16:29:04 -0500

    ready to deploy

--- backend/flask_api/app/run.py
+++
@@ -1,8 +0,0 @@
-import os
-from senses_art import senses_art_app, db
-
-__version__ = "1.0.0"
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    senses_art_app.run(host='0.0.0.0',port=port,debug=True)
